5a1b34246a00abf0c1afdc69a9c0fcdbe3ea22d2
Sunil Jadhav <suniljadhav709@gmail.com>
2023-07-27 12:14:50 -0500

    Subject Vs Observable Sample

--- src/app/subject-vs-observable/subject-vs-observable.component.ts
+++ src/app/subject-vs-observable/subject-vs-observable.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { noop, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-subject-vs-observable',
@@ -6,16 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./subject-vs-observable.component.css']
 })
 export class SubjectVsObservableComponent {
-  subjectData1: string = '';
-  subjectData2: string = '';
-  observableData1: string = '';
-  observableData2: string = '';
+  subjectData1: number| undefined;
+  subjectData2: number| undefined
+  observableData1: number| undefined
+  observableData2: number| undefined
+
+
 
   getObservableData() {
+    let observable$ = new Observable<number>( observer=> {
+      observer.next(Math.random() * 100 + 1)
+    });
 
+    observable$.subscribe(data => { 
+      this.observableData1 = data;
+    });
+
+    observable$.subscribe(data => { 
+      this.observableData2 = data;
+    });
   }
 
   getSubjectData(){
+    let subject$ = new Subject();
 
+    // subject$.subscribe( (data) => {
+    //   this.subjectData1 = <number>data;
+    // }, noop, ()=> console.log)
+
+    // subject$.subscribe( (data) => {
+    //   this.subjectData2 = <number>data;
+    // }, noop, ()=> console.log)
+
+    subject$.subscribe({
+      complete: () => {  }, // completeHandler
+      error: () => {  },    // errorHandler 
+      next: (data) => { this.subjectData1 = <number>data  },     // nextHandler
+    })
+
+    subject$.subscribe({
+      complete: () => {  }, // completeHandler
+      error: () => {  },    // errorHandler 
+      next: (data) => { this.subjectData2 = <number>data  },     // nextHandler
+    })
+    subject$.next(Math.random() * 100 + 1);
   }
 }
